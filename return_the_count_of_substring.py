7d4ec6e69a921e7c96ea81bd41d94c0bbe499bb8
Jossel Redoquerio <josselgredoquerio@iskolarngbayan.pup.edu.ph>
2024-01-22 21:25:15 +0800

    printing

--- return_the_count_of_substring.py
+++ return_the_count_of_substring.py
@@ -1,2 +1,6 @@
 # Exercise 7: Return the count of a given substring from a string
 # Find how many times substring appears in the given string.
+
+str_x = "The cat jumped on the cat, surprising the cat."
+cnt = str_x.count("cat")
+print(cnt)
